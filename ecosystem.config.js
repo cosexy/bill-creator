80d85fbb3ca5571795239373eaf3d81a09710d70
Guen <heloo@guen.dev>
2023-03-11 23:25:27 +0700

    cm

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'http://billcredit.buzz/',
+      name: 'bill-credit',
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: 'npm',
